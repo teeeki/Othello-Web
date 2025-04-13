e82ec6bf6f329332ecc71544f7dcbd775a3fd1ae
tands4247 <tands4247@gmail.com>
2025-04-13 22:51:16 +0900

    first commit

---
+++ src/main/java/com/example/config/PasswordConfig.java
@@ -0,0 +1,26 @@
+package com.othello.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * パスワードエンコーダの設定クラス
+ * パスワードのハッシュ化を行うためのエンコーダをBeanとして提供します
+ */
+@Configuration // Springの設定クラスであることを示すアノテーション
+public class PasswordConfig {
+
+    /**
+     * パスワードエンコーダのBeanを提供します
+     * BCryptアルゴリズムを使用して安全にパスワードをハッシュ化します
+     * 
+     * @return BCryptPasswordEncoderのインスタンス
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCryptは強力なハッシュアルゴリズムでソルトも自動的に生成されます
+        return new BCryptPasswordEncoder();
+    }
+}
